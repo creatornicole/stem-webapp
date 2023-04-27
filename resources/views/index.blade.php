aa33d09f804a60cc05250ab261f1a20d9c1506cf
Nicole Gottschall <ngottschall@web.de>
2023-04-27 10:53:12 +0200

    adds different implementation of bootstrap
    adds needed css and js files of bootstrap for implementation

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,11 +1,18 @@
 <!DOCTYPE html>
 <html lang="de">
-<head>
-    <meta charset="UTF-8">
-    <title>MWC22</title>
-</head>
-<body>
-    <a href="/live">Live</a>
-    <a href="/archive">Archiv</a>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <title>MWC22</title>
+
+        <link rel="stylesheet" href=" {{ asset('css/bootstrap.css') }}">
+        <link rel="stylesheet" href=" {{ asset('css/bootstrap.min.css') }}">
+
+    </head>
+    <body>
+        <a href="/live">Live</a>
+        <a href="/archive">Archiv</a>
+    </body>
+
+    <script src="{{ asset('js/bootstrap.js') }}"></script>
+    <script src="{{ asset('js/bootstrap.min.js') }}"></script>
 </html>
