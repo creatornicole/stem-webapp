de3529c61c832433ffcd52f56555ec8517737630
Nicole Gottschall <ngottschall@web.de>
2023-05-01 16:52:25 +0200

    adds config phpmyadmin to laravel project

--- resources/views/archive.blade.php
+++ resources/views/archive.blade.php
@@ -1,12 +1,38 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>MWC22</title>
+
+    <link rel="stylesheet" href=" {{ asset('css/bootstrap.css') }}">
+    <link rel="stylesheet" href=" {{ asset('css/bootstrap.min.css') }}">
 </head>
 <body>
     <h1>Archive</h1>
+    <?php
+        if(DB::connection()->getPdo()){
+            echo "Successfully connected to DB and DB is ".DB::connection()->getDatabaseName();
+        }
+    ?>
+    <table class="table table-striped">
+        <thead>
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+        </tr>
+        </thead>
+        <tbody>
+        <!-- foreach -->
+        <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+        </tr>
+
+        </tbody>
+    </table>
 </body>
 </html>
