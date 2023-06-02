26597eb07960409e0716c8689e9c15d7409c5c84
Nicole Gottschall <ngottschall@web.de>
2023-06-03 00:35:41 +0200

    adds headings to each column and exports only necessary values of database measurement entries

--- app/Exports/ArchiveMeasurementExport.php
+++ app/Exports/ArchiveMeasurementExport.php
@@ -4,14 +4,30 @@ namespace App\Exports;
 
 use App\Models\Measurement;
 use Maatwebsite\Excel\Concerns\FromCollection;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class ArchiveMeasurementExport implements FromCollection
+class ArchiveMeasurementExport implements FromCollection, ShouldAutoSize, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        return Measurement::where('status', 0)->get();
+        return Measurement::select('id', 'latitude', 'longitude', 'date', 'time', 'ph', 'temp')
+            ->where('status', 0)->get();
+    }
+
+    public function headings(): array 
+    {
+        return [
+            '#',
+            'Latitude',
+            'Longitude',
+            'Datum',
+            'Uhrzeit',
+            'pH',
+            'Temperatur'
+        ];
     }
 }
--- app/Exports/LiveMeasurementExport.php
+++ app/Exports/LiveMeasurementExport.php
@@ -5,14 +5,29 @@ namespace App\Exports;
 use App\Models\Measurement;
 use Maatwebsite\Excel\Concerns\FromCollection;
 use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithHeadings;
 
-class LiveMeasurementExport implements FromCollection, ShouldAutoSize
+class LiveMeasurementExport implements FromCollection, ShouldAutoSize, WithHeadings
 {
     /**
     * @return \Illuminate\Support\Collection
     */
     public function collection()
     {
-        return Measurement::where('status', 1)->get();
+        return Measurement::select('id', 'latitude', 'longitude', 'date', 'time', 'ph', 'temp')
+            ->where('status', 1)->get();
+    }
+
+    public function headings(): array 
+    {
+        return [
+            '#',
+            'Latitude',
+            'Longitude',
+            'Datum',
+            'Uhrzeit',
+            'pH',
+            'Temperatur'
+        ];
     }
 }
